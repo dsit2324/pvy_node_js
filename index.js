430a526169d04dd7f4ee02d4acf3f738f89a0369
ds <148313992+dsit2324@users.noreply.github.com>
2025-03-06 12:18:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ let messages = [];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// const storage = require("./storage/jsonStorage");
+const storage = require("./storage/sqliteStorage.js");
+
 app.post("/send", function(req, res) {    
     messages.push(req.body);
+    console.log(req.ip);
+    storage.addMessage(req.body.username, req.body.message, req.ip);
     console.log(messages);
+    res.redirect("/");
 });
 
 app.get("/messages", function(req, res) {
-    res.json(messages);
+    res.json(storage.getMessages());
+
 });
 
 app.get("/", function (req, res) {
